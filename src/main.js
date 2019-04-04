8302ba38110ef17245dd5f5b3d3edab71fa04265
unknown <angyalva16@gmail.com>
2019-04-04 17:48:11 -0600

    Se modifico la funcion para mostrar los post en pantalla con un server en git bash

--- src/main.js
+++ src/main.js
@@ -1,48 +1,48 @@
 firebase.initializeApp(window.data.config);
 const db = firebase.database();
 //Añadir evento al boton Sign In con correo y contraseña 
-const loginEmail = (email, pass) =>{
+const loginEmail = (email, pass) => {
     firebase.auth().signInWithEmailAndPassword(email, pass)
-    .then((user) => {
-        onNavItemClick('//timeLine');
-        db.ref("/users/" + user.uid).once("value")
-        .then(function(snapshot) {
-            var username = (snapshot.val() && snapshot.val().name)  || 'Anonymous';
-            console.log(snapshot.val())
-            name = username;
-            email1 = email;
-            photo = "https://drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg";
-            showProfile(name, email1, photo);
+        .then((user) => {
+            onNavItemClick('//timeLine');
+            db.ref("/users/" + user.uid).once("value")
+                .then(function (snapshot) {
+                    var username = (snapshot.val() && snapshot.val().name) || 'Anonymous';
+                    console.log(snapshot.val())
+                    name = username;
+                    email1 = email;
+                    photo = "https://drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg";
+                    showProfile(name, email1, photo);
 
+                });
+        })
+        .catch(e => {
+            document.querySelector("#messageEmail").style.display = "block";
+            const message = e.message;
+            document.querySelector("#messageEmail").innerHTML = message;
         });
-    })
-    .catch(e => {
-        document.querySelector("#messageEmail").style.display = "block";
-        const message = e.message;
-        document.querySelector("#messageEmail").innerHTML = message;
-    });
 };
 //Añadir evento al boton Login con correo y contraseña
-const signUp = (email, pass, name1, last) =>{
+const signUp = (email, pass, name1, last) => {
     const nameComplete = name1 + last;
     firebase.auth().createUserWithEmailAndPassword(email, pass)
-    .then(user =>{
-        onNavItemClick('/timeLine');
-        window.data.saveData(user.uid, nameComplete, user.email);
-        name = nameComplete;
-        email1 = email;
-        photo = "https://drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg";
-        post = null;
-        showProfile(name, email1, photo);
-    })
-    .catch(e => {
-        document.querySelector("#messageEmailSU").style.display = "block";
-        const message = e.message;
-        document.querySelector("#messageEmailSU").innerHTML = message;
-    });
+        .then(user => {
+            onNavItemClick('/timeLine');
+            window.data.saveData(user.uid, nameComplete, user.email);
+            name = nameComplete;
+            email1 = email;
+            photo = "https://drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg";
+            post = null;
+            showProfile(name, email1, photo);
+        })
+        .catch(e => {
+            document.querySelector("#messageEmailSU").style.display = "block";
+            const message = e.message;
+            document.querySelector("#messageEmailSU").innerHTML = message;
+        });
 };
 //Añadir evento al boton de Log Out
-const logOut = () =>{
+const logOut = () => {
     document.querySelector("#logIn").style.display = "block";
     firebase.auth().signOut();
 };
@@ -56,22 +56,21 @@ const logOut = () =>{
 //     }else{
 //         btnLogOut.classList.add("hide");
 //     };
-// });
 //Login con google
-const google = () =>{
-    firebase.auth().signInWithPopup(window.data.provider).then(function(result){
+const google = () => {
+    firebase.auth().signInWithPopup(window.data.provider).then(function (result) {
         onNavItemClick('/timeLine');
         window.data.sendDataGoogle(result.user);
         console.log(result.user);
-    return result.user;
-    }).then(()=>{
-        timeLinePosts();
+        return result.user;
+    }).then(() => {
+        timeLinePosts(); 
     });
-    
+
 };
 //Login con Facebook
-const facebook = () =>{
-    firebase.auth().signInWithPopup(window.data.providerFace).then((result) =>{
+const facebook = () => {
+    firebase.auth().signInWithPopup(window.data.providerFace).then((result) => {
         onNavItemClick('/timeLine');
         window.data.sendDataGoogle(result.user);
         console.log(result.user);
@@ -84,54 +83,53 @@ const facebook = () =>{
     });
 };
 //Funcion para mostrar la información del perfil
-const showProfile = (name, email, photo) =>{
+const showProfile = (name, email, photo) => {
     document.querySelector("#profile").innerHTML = ` <img  src="${photo}"> 
     ${name}
     ${email}`;
 };
 //mostrar modal 
-const showModal =() =>{
+const showModal = () => {
     var modal = document.querySelector("#modal");
     modal.style.marginTop = "100px";
-    modal.style.left = ((document.body.clientWidth-350) / 2) +  "px";
+    modal.style.left = ((document.body.clientWidth - 350) / 2) + "px";
     modal.style.display = "block";
 };
 //Almacenar los post en firebase 
-const savePost = () =>{
+const savePost = () => {
     let messagePost = document.querySelector("#post").value;
     window.data.createPost(messagePost);
     document.querySelector("#post").value = "";
 };
 //Mostrar los post en pantalla
-const timeLinePosts = () =>{
-const printPost = firebase.database().ref('posts/');
-printPost.on('child_added', function(s) {
-    const user = s.val();
-    let section = document.createElement("section");
-    console.log(section);
-    let btnUpdate = document.createElement("button");
-    let btnDelete = document.createElement("button");
-    let spanName = document.createElement("span");
-    let span = document.createElement("span");
-    let text = document.createTextNode(user.contenido);
-    let userName = document.createTextNode(user.autor);
-    btnDelete.textContent = 'Eliminar';
-    btnUpdate.textContent = 'Editar';
-    spanName.appendChild(userName);
-    span.appendChild(text);
-    section.appendChild(spanName);
-    section.appendChild(span);
-    section.appendChild(btnUpdate);
-    section.appendChild(btnDelete);
-    console.log(document.querySelector(".postSection"));
-    document.querySelector(".postSection").appendChild(section);
-});
+const timeLinePosts = () => {
+    const printPost = firebase.database().ref('posts/');
+    printPost.on('child_added', function (s) {
+        const baseRoute = document.querySelector("#postSection");
+        const user = s.val();
+        let section = document.createElement("section");
+        let btnUpdate = document.createElement("button");
+        let btnDelete = document.createElement("button");
+        let spanName = document.createElement("span");
+        let span = document.createElement("span");
+        let text = document.createTextNode(user.contenido);
+        let userName = document.createTextNode(user.autor);
+        btnDelete.textContent = 'Eliminar';
+        btnUpdate.textContent = 'Editar';
+        spanName.appendChild(userName);
+        span.appendChild(text);
+        section.appendChild(spanName);
+        section.appendChild(span);
+        section.appendChild(btnUpdate);
+        section.appendChild(btnDelete);
+        baseRoute.appendChild(section);
+    })
 };
 //Quitar las comillas a la las rutas 
 const socialNetwork = {
-    "loginEmail" : loginEmail,
-    "signUp" : signUp,
-    "google" : google,
+    "loginEmail": loginEmail,
+    "signUp": signUp,
+    "google": google,
     "facebook": facebook,
     "timeLine": timeLinePosts,
 };
